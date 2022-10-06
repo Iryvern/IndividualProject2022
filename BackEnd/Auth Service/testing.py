007b025d4f48ee9192b1cefba435d0332d69755f
Kirill Smirnov <kirilsmirnov@livewallgroup.com>
2022-10-06 12:59:26 +0200

    Check

--- BackEnd/Auth Service/testing.py
+++ BackEnd/Auth Service/testing.py
@@ -1,14 +1,9 @@
-import unittest
 from encryption import *
 
 
-class TestEncryption(unittest.TestCase):
+class TestEncryption:
     def test_encrypt(self):
         var = "Test"
         e_var = encrypt_data(var)
         result = compare_e_data(var, e_var)
-        self.assertEqual(result, True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert result == True
