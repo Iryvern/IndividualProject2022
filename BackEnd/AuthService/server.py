5492f26df0f082499444ba4de4006c2b4ad4b1b4
Kirill Smirnov <kirilsmirnov@livewallgroup.com>
2022-10-12 16:18:25 +0200

    Removed csrf

--- BackEnd/AuthService/server.py
+++ BackEnd/AuthService/server.py
@@ -5,14 +5,12 @@ from authentification import *
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-from flask_wtf.csrf import CSRFProtect
 from waitress import serve
+import os
 
-white = ["localhost:3000"]
+white = ["*"]
 
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app)
 CORS(app, resources={r"/*": {"origins": white, "send_wildcard": "False"}})
 api = Api(app)
 app.config["JWT_SECRET_KEY"] = "ZOl9P^8Ag9K6O2JCmjc&"  # Hide this!
