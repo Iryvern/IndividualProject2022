b2c984b48d54026ca39e7072fa0507ef278c2d68
Kirill Smirnov <kirilsmirnov@livewallgroup.com>
2022-11-03 10:01:16 +0100

    Seperated the databases

--- BackEnd/CommunityService/database.py
+++ BackEnd/CommunityService/database.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 cluster = MongoClient(
     "mongodb+srv://Kirill:bjObxVisIWqE0Gyr@cluster0.5m5xlod.mongodb.net/?retryWrites=true&w=majority")
 
-db = cluster["IndividualDataDB"]
+db = cluster["IndividualDataDB2"]
 collection = db["Community"]
 
 
